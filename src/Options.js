c6cb9ebe428c56da127fd3ecda19b11fcf923a14
Damian Ludef <damiinho@gmail.com>
2023-08-30 14:10:58 +0200

    options show/hide + background

--- src/Options.js
+++ src/Options.js
@@ -1,11 +1,35 @@
 import AddParty from "./AddParty";
 import AddDistrict from "./AddDistrict";
+import { useContext } from "react";
+import { AppContext } from "./contexts/AppContext";
+import showIMG from "./img/show.png";
+import hideIMG from "./img/hide.png";
 
 const Options = () => {
+  const { showOptions, setShowOptions } = useContext(AppContext);
+
+  const handleShowOptions = () => {
+    setShowOptions(!showOptions);
+  };
+
   return (
-    <div className="options">
-      <AddParty />
-      <AddDistrict />
+    <div className="App__options">
+      <div className="App__options-handler" onClick={handleShowOptions}>
+        <span className="App__options-handler__name">Ustawienia</span>
+        <img
+          className="App__options-handler__img"
+          src={showOptions ? showIMG : hideIMG}
+          alt="show"
+        />
+      </div>
+      {showOptions ? (
+        <div className="options">
+          <AddParty />
+          <AddDistrict />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -5,12 +5,15 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [parties, setParties] = useState([]);
   const [districts, setDistricts] = useState([]);
+  const [showOptions, setShowOptions] = useState(true);
 
   const providerValue = {
     parties,
     setParties,
     districts,
     setDistricts,
+    showOptions,
+    setShowOptions,
   };
 
   return (
