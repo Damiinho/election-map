0cea0cfc85195986da679593b6542c340d3fb44b
Damian Ludef <damiinho@gmail.com>
2023-09-08 16:29:46 +0200

    districts fill fix

--- src/District.js
+++ src/District.js
@@ -17,7 +17,7 @@ const District = (props) => {
   const [addLocal, setAddLocal] = useState(false);
   const [name, setName] = useState("");
   const [currentResults, setCurrentResults] = useState(
-    Array(props.deputies).fill(0)
+    Array(districts[props.index].parties.length).fill(0)
   );
 
   const currentDistrict = districts[props.index];
@@ -76,6 +76,8 @@ const District = (props) => {
 
       return updatedDistricts;
     });
+    const results = districts[props.index].parties.map((party) => party.result);
+    setCurrentResults(results);
   };
 
   const handleStart = () => {
@@ -174,6 +176,40 @@ const District = (props) => {
     newResults[index] = currentValue;
     setCurrentResults(newResults);
   };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   return (
     <div
       className={`districts__element ${
@@ -342,29 +378,38 @@ const District = (props) => {
         </Button>
       </div>
 
-      <div className="districts__element-doughnut">
-        {currentDistrict.showFinalResult ? (
-          <Doughnut
-            className="districts__element-doughnut__item"
-            data={currentDistrict.forChart}
-            style={{ cursor: "pointer" }}
-            options={{
-              responsive: true,
-              plugins: {
-                legend: {
-                  position: "bottom",
-                  labels: {
-                    color: "white",
-                    letterSpacing: "10px",
+      {currentDistrict.showFinalResult ? (
+        <div className="districts__element-doughnut">
+          <div className="districts__element-doughnut__description">
+            {currentDistrict.finalResult.map((item) => (
+              <div>
+                {item.name}, wynik: {item.result}, miejsca: {item.seats}
+              </div>
+            ))}
+          </div>
+
+          <div className="districts__element-doughnut__item">
+            <Doughnut
+              data={currentDistrict.forChart}
+              style={{ cursor: "pointer" }}
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: "bottom",
+                    labels: {
+                      color: "white",
+                      letterSpacing: "10px",
+                    },
                   },
                 },
-              },
-            }}
-          ></Doughnut>
-        ) : (
-          ""
-        )}
-      </div>
+              }}
+            ></Doughnut>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
