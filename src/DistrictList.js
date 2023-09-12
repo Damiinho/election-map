e3ae5180026596c247a88c0c40c6b6a74edd8587
Damian Ludef <damiinho@gmail.com>
2023-09-12 02:14:01 +0200

    show/hide districts

--- src/DistrictList.js
+++ src/DistrictList.js
@@ -1,24 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "./contexts/AppContext";
 import District from "./District";
 import DistrictListButton from "./DistrictListButtons";
+import MySwitch from "./MySwitch";
 
 const DistrictList = () => {
   const { districts } = useContext(AppContext);
+  const [showDistricts, setShowDistricts] = useState(true);
+  const handleShowDistricts = () => {
+    setShowDistricts(!showDistricts);
+  };
 
   return (
     <div className="districts">
-      {districts.length > 0 && <DistrictListButton />}
-      <div className="districts-list">
-        {districts.map((item, index) => (
-          <District
-            key={index}
-            name={item.name}
-            deputies={item.deputies}
-            method={item.method}
-            index={index}
+      <div className="districts-title">
+        <p>Okręgi</p>
+        <div className="districts-title__side">
+          <MySwitch
+            onClick={handleShowDistricts}
+            imgDisplay
+            thumbDisplay={false}
+            defaultValue
           />
-        ))}
+        </div>
+      </div>
+      <div className={`districts-wrapper ${showDistricts ? "" : "hide"}`}>
+        {/* <DistrictListButton /> */}
+        <div className="districts-list">
+          {districts.map((item, index) => (
+            <District
+              key={index}
+              name={item.name}
+              deputies={item.deputies}
+              method={item.method}
+              index={index}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
