0f6a437fe65b885a6a881e6a633783732cfba70f
Damian Ludef <damiinho@gmail.com>
2023-09-14 14:20:20 +0200

    make myBar independent

--- src/MyBar.js
+++ src/MyBar.js
@@ -6,15 +6,59 @@ const MyBar = (props) => {
     0
   );
 
+  const myBarStyle = {
+    margin: "0 auto",
+    width: "90%",
+    borderRadius: 7,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const wrapperStyle = {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    overflow: "hidden",
+    borderRadius: 12,
+  };
+  const wrapperCenterStyle = {
+    width: "3px",
+    height: "100%",
+    backgroundColor: "#ffffff48",
+    backgroundImage:
+      "linear-gradient(0deg, #ffffff 0%, #ffffff 12.5%, transparent 12.5%, transparent 25%, #ffffff 25%, #ffffff 37.5%, transparent 37.5%, transparent 50%, #ffffff 50%, #ffffff 62.5%, transparent 62.5%, transparent 75%, #ffffff 75%, #ffffff 87.5%, transparent 87.5%, transparent 100%)",
+    position: "absolute",
+    top: "0%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    cursor: "pointer",
+  };
+
+  const itemColor = {
+    display: "flex",
+    height: "30px",
+    fontSize: "12px",
+    fontFamily: '"MuseoModerno", cursive',
+    color: "rgb(235, 235, 235)",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
-    <div className="list__element-bars__bar">
+    <div className="myBar" style={myBarStyle}>
       <Tooltip id="myBar-tooltip" />
       {props.name}
-      <div className="list__element-bars__bar-wrapper">
+      <div className="myBar-wrapper" style={wrapperStyle}>
         <div
-          className="list__element-bars__bar-wrapper__center"
+          className="myBar-wrapper__center"
           data-tooltip-id="my-tooltip"
           data-tooltip-content="50%"
+          style={wrapperCenterStyle}
         ></div>
         {props.result.map((item) => {
           if (item[props.value] === 0) return null;
@@ -26,14 +70,17 @@ const MyBar = (props) => {
           return (
             <div
               key={item.color}
-              className="list__element-bars__bar-wrapper__item"
-              style={{ width: `${(item[props.value] / sum) * 100}%` }}
+              className="myBar-wrapper__item"
+              style={{
+                width: `${(item[props.value] / sum) * 100}%`,
+                cursor: "pointer",
+              }}
               data-tooltip-id="myBar-tooltip"
               data-tooltip-content={`${item.name}, ${itemValue}`}
             >
               <div
-                className="list__element-bars__bar-wrapper__item-color"
-                style={{ backgroundColor: `${item.color}` }}
+                className="myBar-wrapper__item-color"
+                style={{ ...itemColor, backgroundColor: `${item.color}` }}
               >
                 {item.result / sum > 0.03 && <span>{itemValue}</span>}
               </div>
