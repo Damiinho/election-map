9c02f55fcd03d3c46fd4fe59b9eef5062c6164da
Damian Ludef <damiinho@gmail.com>
2023-09-26 18:21:10 +0200

    - useState

--- src/Summary/SimpleSummaryDistrict.js
+++ src/Summary/SimpleSummaryDistrict.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { DataContext } from "../contexts/DataContext";
 import MySmallInfoBox from "../Components/MySmallInfoBox";
 import MyBar from "../Components/MyBar";
