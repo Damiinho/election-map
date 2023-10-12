5d5da9341f94d820e882aca0c90bdc0900e16978
Damian Ludef <damiinho@gmail.com>
2023-10-13 01:25:26 +0200

    + key fix

--- src/Test/Questions.js
+++ src/Test/Questions.js
@@ -10,52 +10,46 @@ const Questions = () => {
     <>
       {questions.map((item, index) => {
         return (
-          <>
-            <div className="test__list-item" key={index}>
-              {" "}
-              {windowWidth > 550 && (
-                <div className="test__list-item__number">{index + 1}.</div>
-              )}
-              {/* <div
+          <div className="test__list-item" key={index}>
+            {windowWidth > 550 && (
+              <div className="test__list-item__number">{index + 1}.</div>
+            )}
+            {/* <div
                 className="test__list-item__result"
                 style={{ backgroundColor: result() }}
               ></div> */}
-              <div className="test__list-item__question">
-                {!(windowWidth > 550) && <span>{index + 1}.</span>}
-                <span>{item.question}</span>
-              </div>
-              <div className="test__list-item__category">
-                <span>Wagi:</span>
-                <span> Socjalizm / Kapitalizm: {item.effects.right || 0}</span>
-                <span> Anarchizm / Autorytaryzm: {item.effects.auth || 0}</span>
-                <span> Tradycja / Postęp: {item.effects.prog || 0}</span>
-              </div>
-              <div className="test__list-item__answers">
-                <span>
-                  Od {item.min ? item.min : item.min === 0 ? "0" : "-1"}
-                </span>
-                <div>
-                  {item.marks ? (
-                    item.marks.map((mark) => (
-                      <span style={{ padding: "0 5" }}>
-                        {mark.value} {mark.answer}
-                      </span>
-                    ))
-                  ) : (
-                    <>
-                      <span>-0.99 Nie zgadzam się</span>
-                      <span>0 Nie wiem</span>
-                      <span>0.99 Zgadzam się</span>
-                    </>
-                  )}
-                </div>
-                <span>
-                  do {item.max ? item.max : item.max === 0 ? "0" : "1"}
-                </span>
-              </div>
+            <div className="test__list-item__question">
+              {!(windowWidth > 550) && <span>{index + 1}.</span>}
+              <span>{item.question}</span>
             </div>
-            {/* <div className="test__list-item">{item.answer.map}</div> */}
-          </>
+            <div className="test__list-item__category">
+              <span>Wagi:</span>
+              <span> Socjalizm / Kapitalizm: {item.effects.right || 0}</span>
+              <span> Anarchizm / Autorytaryzm: {item.effects.auth || 0}</span>
+              <span> Tradycja / Postęp: {item.effects.prog || 0}</span>
+            </div>
+            <div className="test__list-item__answers">
+              <span>
+                Od {item.min ? item.min : item.min === 0 ? "0" : "-1"}
+              </span>
+              <div>
+                {item.marks ? (
+                  item.marks.map((mark, index) => (
+                    <span key={index} style={{ padding: "0 5" }}>
+                      {mark.value} {mark.answer}
+                    </span>
+                  ))
+                ) : (
+                  <>
+                    <span>-0.99 Nie zgadzam się</span>
+                    <span>0 Nie wiem</span>
+                    <span>0.99 Zgadzam się</span>
+                  </>
+                )}
+              </div>
+              <span>do {item.max ? item.max : item.max === 0 ? "0" : "1"}</span>
+            </div>
+          </div>
         );
       })}
     </>
