811f50faee8024150b7842e47fe0de8fb39c6e63
Damian Ludef <damiinho@gmail.com>
2023-10-27 19:11:28 +0200

    redesining buttons

--- src/Test/ResultsPanel.js
+++ src/Test/ResultsPanel.js
@@ -2,8 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { TestContext } from "../contexts/TestContext";
 import { AppContext } from "../contexts/AppContext";
-import { Button, Slider } from "@mui/material";
+import { Button, ButtonGroup, Menu, MenuItem, Slider } from "@mui/material";
 import TestResultBox from "./TestResultBox";
+import CubeWhiteIMG from "../img/cubeWhite.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import SimpleLogo2 from "../img/calculatorsimplelogo2.png";
 
 const ResultsPanel = () => {
   const [officialResult, setOfficialResult] = useState({});
@@ -12,7 +17,102 @@ const ResultsPanel = () => {
   const { windowWidth } = useContext(AppContext);
   const params = useParams();
   const navigate = useNavigate();
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const buttons = [
+    <Button
+      size="small"
+      key="test"
+      style={{
+        color: "white",
+        textTransform: "lowercase",
+        fontFamily: "Ysabeau Office, sans-serif",
+        fontWeight: 600,
+        gap: 10,
+      }}
+      onClick={() => {
+        handleClose();
+        navigate("/test/");
+        setIsTestStart(false);
+        setCurrentQuestion(0);
+      }}
+    >
+      <span
+        style={{
+          justifyContent: "right",
+          alignItems: "center",
+          width: "100%",
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <span style={{ position: "relative", top: 2 }}>od nowa</span>
+        <RestartAltIcon />
+      </span>
+    </Button>,
+    <Button
+      size="small"
+      key="list"
+      style={{
+        color: "white",
+        textTransform: "lowercase",
+        fontFamily: "Ysabeau Office, sans-serif",
+        fontWeight: 600,
+        gap: 10,
+      }}
+      onClick={() => {
+        navigate("/test/lista");
+      }}
+    >
+      <span
+        style={{
+          justifyContent: "right",
+          alignItems: "center",
+          width: "100%",
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <span style={{ position: "relative", top: 2 }}>lista pytań</span>
+        <FormatListBulletedIcon fontSize="medium" />
+      </span>
+    </Button>,
+    <Button
+      size="small"
+      key="simple"
+      style={{
+        color: "white",
+        textTransform: "lowercase",
+        fontFamily: "Ysabeau Office, sans-serif",
+        fontWeight: 600,
+        gap: 10,
+      }}
+      onClick={() => {
+        navigate("/prosty/sejm");
+      }}
+    >
+      <span
+        style={{
+          justifyContent: "right",
+          alignItems: "center",
+          width: "100%",
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <span style={{ position: "relative", top: 2 }}>
+          kalkulator wyborczy
+        </span>
+        <img src={SimpleLogo2} alt="logo" style={{ height: 25 }} />
+      </span>
+    </Button>,
+  ];
   useEffect(() => {
     if (params.values) {
       const resultArray = params.values.split("+");
@@ -126,46 +226,138 @@ const ResultsPanel = () => {
 
   return (
     <div className="test">
-      <div className="test__title">Polityczna bryła – wynik</div>
-      <div className="test__result">
-        <div className="test__result-buttons">
-          <Button
-            variant="contained"
-            // style={{ backgroundColor: "#f03b3b" }}
-            color="info"
-            size={windowWidth > 550 ? "medium" : "small"}
-            onClick={() => {
-              navigate("/test/lista");
-            }}
-          >
-            lista pytań
-          </Button>
-          <Button
-            variant="contained"
-            // style={{ backgroundColor: "#f03b3b" }}
-            color="info"
-            size={windowWidth > 550 ? "medium" : "small"}
-            onClick={() => {
-              navigate("/test/");
+      <div className="test__title">
+        {" "}
+        <div className="test__title-text">
+          Polityczna bryła – wynik
+          <img
+            style={{ height: windowWidth > 800 ? 80 : 60 }}
+            src={CubeWhiteIMG}
+            alt=""
+          />
+          {windowWidth > 800 ? (
+            <ButtonGroup
+              aria-label="vertical contained button group"
+              variant="text"
+              orientation="vertical"
+              style={{
+                position: "absolute",
+                bottom: 5,
+                right: 5,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "left",
+                alignItems: "left",
+              }}
+            >
+              {buttons}
+            </ButtonGroup>
+          ) : null}
+          {windowWidth > 800 ? null : (
+            <div style={{ position: "absolute", bottom: 0, right: 0 }}>
+              <Button
+                id="positioned-button"
+                aria-controls={open ? "positioned-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                style={{ color: "white" }}
+              >
+                <MenuIcon fontSize="large" />
+              </Button>
+              <Menu
+                id="positioned-menu"
+                aria-labelledby="positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+              >
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate("/test/");
+                    setIsTestStart(false);
+                    setCurrentQuestion(0);
+                  }}
+                  style={{ gap: 10, width: "100%" }}
+                >
+                  <span
+                    style={{
+                      justifyContent: "right",
+                      alignItems: "center",
+                      width: "100%",
+                      display: "flex",
+                      gap: 10,
 
-              setIsTestStart(false);
-              setCurrentQuestion(0);
-            }}
-          >
-            strona główna testu
-          </Button>
-          <Button
-            variant="contained"
-            // style={{ backgroundColor: "#f03b3b" }}
-            color="info"
-            size={windowWidth > 550 ? "medium" : "small"}
-            onClick={() => {
-              navigate("/prosty/");
-            }}
-          >
-            kalkulator
-          </Button>
+                      fontFamily: "Ysabeau Office, sans-serif",
+                    }}
+                  >
+                    <span style={{ position: "relative", top: 2 }}>
+                      od nowa
+                    </span>
+                    <RestartAltIcon />
+                  </span>
+                </MenuItem>
+
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+
+                    navigate("/test/lista");
+                  }}
+                >
+                  <span
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      gap: 10,
+                      justifyContent: "right",
+                      alignItems: "center",
+                      fontFamily: "Ysabeau Office, sans-serif",
+                    }}
+                  >
+                    lista pytań
+                    <FormatListBulletedIcon fontSize="medium" />
+                  </span>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate("/prosty/sejm");
+                  }}
+                  style={{ gap: 10, width: "100%" }}
+                >
+                  <span
+                    style={{
+                      justifyContent: "right",
+                      alignItems: "center",
+                      width: "100%",
+                      display: "flex",
+                      gap: 10,
+
+                      fontFamily: "Ysabeau Office, sans-serif",
+                    }}
+                  >
+                    <span style={{ position: "relative", top: 2 }}>
+                      kalkulator wyborczy
+                    </span>
+                    <img src={SimpleLogo2} alt="logo" style={{ height: 25 }} />
+                  </span>
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
         </div>
+      </div>
+      <div className="test__result">
         <div className="test__result-description">
           {" "}
           <div className="test__result-description__title">
