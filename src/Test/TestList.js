6762b5e866fb0493375b80bccaf7d87d82331789
Damian Ludef <damiinho@gmail.com>
2023-10-28 14:14:35 +0200

    clearing

--- src/Test/TestList.js
+++ src/Test/TestList.js
@@ -2,35 +2,15 @@ import { Button } from "@mui/material";
 import Questions from "./Questions";
 import ReplySharpIcon from "@mui/icons-material/ReplySharp";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AppContext } from "../contexts/AppContext";
 
 const TestList = () => {
   const navigate = useNavigate();
-  const { windowWidth } = useContext(AppContext);
   return (
     <div className="test">
       <div className="test__list">
         <div className="test__list-title">
           <p>lista pytań</p>
-          {/* <Button
-            variant="contained"
-            color="info"
-            size="small"
-            style={{
-              textTransform: "lowercase",
-              position: windowWidth > 450 ? "absolute" : "static",
-              bottom: windowWidth > 450 ? 10 : "auto",
-              right: windowWidth > 450 ? 10 : "auto",
-              margin: "0 auto",
-            }}
-            onClick={() => {
-              navigate(-1);
-            }}
-            startIcon={<ReplySharpIcon />}
-          >
-            powrót
-          </Button> */}
+
           <Button
             size="small"
             style={{
